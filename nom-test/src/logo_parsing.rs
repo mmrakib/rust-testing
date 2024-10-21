6f467c76839c0f44a2564cc1c3bc784322fc1c9a
mmrakib <mohammadmrakib@gmail.com>
2024-10-21 12:27:01 +1100

    Added whitespace and comment parsing tests for the Logo programming language

---
+++ nom-test/src/logo_parsing.rs
@@ -0,0 +1,56 @@
+#[allow(unused_imports)]
+#[allow(unused_variables)]
+
+use nom::{
+    branch::alt, bytes::complete::{is_not, tag, tag_no_case, take_while1},
+    character::complete::{alpha1, alphanumeric1, digit1, multispace0, multispace1, one_of, line_ending, not_line_ending},
+    combinator::{map, map_res, opt, recognize},
+    multi::{many0, many1, separated_list0},
+    sequence::{delimited, preceded, separated_pair, terminated, tuple},
+    Err, IResult, UnspecializedInput,
+};
+
+type ParserError<'a> = nom::error::VerboseError<&'a str>;
+
+fn parse_whitespace(input: &str) -> IResult<&str, &str, ParserError> {
+    map(multispace0, |x| x)(input)
+}
+
+fn parse_comment(input: &str) -> IResult<&str, &str, ParserError> {
+    preceded(
+        tag("//"),
+        not_line_ending
+    )(input).and_then(|(next_input, comment)| {
+        match line_ending::<&str, ParserError>(next_input) {
+            Ok((remaining, _)) => Ok((remaining, comment)),
+            Err(nom::Err::Error(_)) => Ok((next_input, comment)),
+            Err(e) => Err(e),
+        }
+    })
+}
+
+pub fn observe_parse_whitespace() {
+    let input: &str = "          \n";
+
+    match parse_whitespace(input) {
+        Ok(output) => {
+            println!("{:?}", output);
+        },
+        Err(error) => {
+            panic!("{:?}", error);
+        }
+    };
+}
+
+pub fn observe_parse_comment() {
+    let input: &str = "// Hello, this is a comment.\n";
+
+    match parse_comment(input) {
+        Ok(output) => {
+            println!("{:?}", output);
+        },
+        Err(error) => {
+            panic!("{:?}", error);
+        }
+    }
+}
--- nom-test/src/main.rs
+++ nom-test/src/main.rs
@@ -1,22 +1,13 @@
-use nom::{
-    IResult,
-    character::complete::{alpha1, space0},
+#[allow(unused_imports)]
+#[allow(unused_variables)]
+
+mod logo_parsing;
+use crate::logo_parsing::{
+    observe_parse_whitespace,
+    observe_parse_comment,
 };
 
-fn parse_command_name(input: &str) -> IResult<&str, &str> {
-    alpha1(input)
-}
-
-fn parse_space(input: &str) -> IResult<&str, &str> {
-    space0(input)
-}
-
 fn main() {
-    let input = "abc def";
-    let result = parse_command_name(input).unwrap();
-    let result2 = parse_space(result.0).unwrap();
-    let result3 = parse_command_name(result2.0).unwrap();
-
-    println!("Result 1: {:?}", result);
-    println!("Result 2: {:?}", result3);
+    observe_parse_whitespace();
+    observe_parse_comment();  
 }
